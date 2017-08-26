ad4b2395ecafed392e914049047c455de178e1d0
Austin Martinez <ausmartinez@csumb.edu>
2017-08-26 21:11:10 +0000

    adding files to use in class

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
 <html>
     <head>
         <title>ePortfolio</title>
         <meta charset="utf-8"/>
     </head>
     <body>
-        <h1>Welcome to this website!</h1>
+        <div class="container">
+            <h1>Austin Martinez</h1>
+            <nav>
+                <div class="nav-element highlight">Home</div>
+                <div class="nav-element">About</div>
+                <div class="nav-element">Contact</div>
+            </nav>
+            <img id="picture" src="source" alt="picture-alt">
+            <article>Stuff goes here</article>
+        </div>
     </body>
 </html>
