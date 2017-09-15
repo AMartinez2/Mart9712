0816f9021394a2e3551943ee770e828d3d40c6bb
Austin Martinez <ausmartinez@csumb.edu>
2017-09-15 04:17:03 +0000

    Adding homework 2 files

--- homework-2/inc/functions.php
+++ homework-2/inc/functions.php
@@ -1,23 +1,35 @@
 <?php
     function run(){
-        for ($i=1; $i<5; $i++){
+        // Randomly select font
+        $var1 = rand(0, 3);
+        echo "<div class='page-title font$var1'><h1>CHOOSE YOUR DESTINY</h1>";
+        echo "<div class='inside font$var1'>";
+        
+        for ($i=1; $i<7; $i++){
             ${"array".$i} = createArrayFromFile("names/names".$i.".txt");
             shuffle(${"array".$i});
         }
-        for($i = 0; $i < 2; $i++){
+        for($i = 0; $i < 3; $i++){
             if($i == 0){
-                echo "<h1>YOU ARE</h1>";
+                echo "<h3>YOU ARE:</h3>";
                 $var1 = array_rand($array1);
                 $var2 = array_rand($array2);
-                echo "<h2>".$array1[$var1]." the ".$array2[$var2]."</h2>";
+                echo "<h1>".$array1[$var1]." ".$array2[$var2]."</h1>";
             }
-            else {
-                echo "<h1>DESTINED TO</h1>";
+            if($i == 1) {
+                echo "<h3>THE</h3>";
                 $var1 = array_rand($array3);
                 $var2 = array_rand($array4);
-                echo "<h2>".$array3[$var1]." the ".$array4[$var2]."</h2>";
+                echo "<h1>".$array3[$var1]." ".$array4[$var2]."</h1>";
+            }
+            else if($i == 2) {
+                echo "<h3>DESTINED TO</h3>";
+                $var1 = array_rand($array5);
+                $var2 = array_rand($array6);
+                echo "<h1>".$array5[$var1]." ".$array6[$var2]."</h1>";
             }
         }
+        echo "</div>";
     }
     
     
--- homework-2/index.php
+++ homework-2/index.php
@@ -6,7 +6,10 @@
     <head>
         <meta charset='utf-8'>
         <title>Choose Your Destiny</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <style>
+            @import url("css/styles.css");
+        </style>
+        <link href="https://fonts.googleapis.com/css?family=Anton|Raleway|Yanone+Kaffeesatz" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body class='container'>
