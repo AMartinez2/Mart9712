07beeda687aa0c5ab7e7656b667d9c4c90602192
Austin Martinez <ausmartinez@csumb.edu>
2017-10-14 08:45:14 +0000

    Trying to change connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,21 +1,24 @@
 <?php
 function getDatabaseConnection() {
     
-    $host = 'localhost'; // C9 host
-    $dbname = 'tcp';
-    $username = 'root';
-    $password = '';
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
     //using different database variables in Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
-        $dbname = substr($url["path"], 1);
+        // $dbname = substr($url["path"], 1);
+        $dbname = 'heroku_7a3de33b4ce1274';
         $username = $url["user"];
         $password = $url["pass"];
-    } 
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } else {
+        $host = 'localhost'; // C9 host
+        $dbname = 'tcp';
+        $username = 'root';
+        $password = '';
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
     
     return $dbConn;
 }
